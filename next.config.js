3dec01debdf8d2f975f0741fb64e75da35c52a4e
mehraankush <mehraankush36@gmail.com>
2023-09-18 01:21:33 +0530

    added build ignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{domains:["lh3.googleusercontent.com"]}
+    images:{domains:["lh3.googleusercontent.com"]},
+    typescript: {
+        ignoreBuildErrors: true,
+      },
 }
 
 module.exports = nextConfig
